22c3e3645dc8995648cd0921ac6851c7a5d33041
shinjith-dev <shinjithpallikkara@gmail.com>
2022-09-18 18:22:59 +0530

    part1-1.4

--- part1/courseinfo1.2/src/components/Total.js
+++ part1/courseinfo1.2/src/components/Total.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-const Total = (props) => {
+const Total = ({ parts }) => {
   return (
     <p>
       Number of exercises{" "}
-      {props.exercises1 + props.exercises2 + props.exercises3}
+      {parts[0].exercises + parts[1].exercises + parts[2].exercises}
     </p>
   );
 };
