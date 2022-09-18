4f3dab61c9469cbf7e317a2cc6da76ae581db94c
shinjith-dev <shinjithpallikkara@gmail.com>
2022-09-18 17:56:33 +0530

    part1-1.2

--- part1/courseinfo1.2/src/App.js
+++ part1/courseinfo1.2/src/App.js
@@ -1,6 +1,6 @@
 import Content from "./components/Content";
 import Header from "./components/Header";
-import Total from "./Total";
+import Total from "./components/Total";
 
 const App = () => {
   const course = "Half Stack application development";
