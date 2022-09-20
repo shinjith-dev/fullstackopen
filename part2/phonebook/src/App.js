251814513383fdb619b3d8b09fad4a4c6c0de9c3
shinjith-dev <shinjithpallikkara@gmail.com>
2022-09-21 00:13:32 +0530

    courseinfo 2.7

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (
+      !persons.some(
+        (person) => JSON.stringify(person) === JSON.stringify({ name: newName })
+      )
+    ) {
+      setPersons(persons.concat({ name: newName }));
+      setNewName("");
+    } else {
+      alert(`${newName} is already added to phonebook`);
+      setNewName("");
+    }
+  };
+
+  const handleInputChange = (e) => {
+    setNewName(e.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          name: <input value={newName} onChange={handleInputChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <p key={person.name}>{person.name}</p>
+      ))}
+    </div>
+  );
+};
+
+export default App;
