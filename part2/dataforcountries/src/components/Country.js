31735770dacef223542efb3a15104c346420c9dd
shinjith-dev <shinjithpallikkara@gmail.com>
2022-09-21 15:37:49 +0530

    dataforcountries 2.14

--- part2/dataforcountries/src/components/Country.js
+++ part2/dataforcountries/src/components/Country.js
@@ -1,9 +1,31 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 const Country = ({ country }) => {
+  const [weatherFetched, setWeatherFetched] = useState(false);
+  const [weather, setWeather] = useState({});
+  useEffect(() => {
+    axios
+      .get(
+        `http://api.openweathermap.org/geo/1.0/direct?q=${country.capital[0]},${country.cca2}&limit=1&appid=${API_KEY}`
+      )
+      .then((res) => {
+        axios
+          .get(
+            `https://api.openweathermap.org/data/2.5/weather?lat=${res.data[0].lat}&lon=${res.data[0].lon}&appid=${API_KEY}&units=metric`
+          )
+          .then((res) => {
+            setWeatherFetched(true);
+            setWeather(res.data);
+          });
+      });
+  }, []);
+
   return (
     <>
-      <h2>{country.name.common}</h2>
+      <h1>{country.name.common}</h1>
       <p>capital: {country.capital.map((capital) => capital + " ")}</p>
       <p>area: {country.area}</p>
       <h4>languages:</h4>
@@ -12,7 +34,17 @@ const Country = ({ country }) => {
           <li key={lang}>{lang}</li>
         ))}
       </ul>
-      <img src={country.flags.png} />
+      <img src={country.flags.png} alt={`${country.name.common}-flag`} />
+      <h2>Weather in {country.capital[0]}</h2>
+      {weatherFetched && (
+        <>
+          <p>temperature: {weather.main.temp} Celcius</p>
+          <img
+            src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+          ></img>
+          <p>wind: {weather.wind.speed} m/s</p>
+        </>
+      )}
     </>
   );
 };
