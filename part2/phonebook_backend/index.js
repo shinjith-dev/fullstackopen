180dcef6a4ab65c7e585b0961e59b5cc690b1868
shinjith-dev <shinjithpallikkara@gmail.com>
2022-09-23 17:35:15 +0530

    phonebook_backend 3.6

--- part2/phonebook_backend/index.js
+++ part2/phonebook_backend/index.js
@@ -67,9 +67,18 @@ app.post("/api/persons", (req, res) => {
 
   if (!body.name) {
     return res.status(400).json({
-      error: "content missing",
+      error: "name is missing",
+    });
+  } else if (!body.number) {
+    return res.status(400).json({
+      error: "number is missing",
     });
   }
+
+  if (persons.filter((person) => person.name === body.name).length !== 0)
+    return res.status(400).json({
+      error: "name must be unique",
+    });
 
   const person = {
     id: generateId(),
