ed202839bbe9da0474a7a80b04abfc2fd83d3809
shinjith-dev <shinjithpallikkara@gmail.com>
2023-05-24 15:23:48 +0530

    bloglist 4.5

--- part4/bloglist/tests/list_helper.test.js
+++ part4/bloglist/tests/list_helper.test.js
@@ -5,19 +5,60 @@ test('dummy returns one', () => {
 })
 
 describe('total likes', () => {
-  const listWithOneBlog = [
-    {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-      likes: 5,
-      __v: 0
-    }
-  ]
-
   test('when list has only one blog, equals the likes of that', () => {
     const result = listHelper.totalLikes(listWithOneBlog)
     expect(result).toBe(5)
   })
 })
+
+describe('[listhelpertest] favorite blog', () => {
+  test('most liked of 3 blogs', () => {
+    const result = listHelper.favoriteBlog(listWithThreeBlog)
+    expect(result).toEqual({
+      _id: '5a422aa71b54a6d89234d17f8',
+      title: 'Go To Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    })
+  })
+})
+
+const listWithOneBlog = [
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0
+  }
+]
+
+const listWithThreeBlog = [
+  {
+    _id: '5a422aa71b54a62378yh234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 1,
+    __v: 0
+  },
+  {
+    _id: '5a422aa71b54a6d89234d17f8',
+    title: 'Go To Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0
+  },
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Better don\'t say',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 3,
+    __v: 0
+  }
+]
--- part4/bloglist/util/list_helper.js
+++ part4/bloglist/util/list_helper.js
@@ -1,12 +1,24 @@
 // eslint-disable-next-line no-unused-vars
 const dummy = (blogs) => {
-    return 1
+  return 1
 }
 
-const totalLikes = (blogs)=>{
-    return blogs.length===0 ? 0 : blogs.reduce((prev,curr)=>prev+curr.likes,0)
+const totalLikes = (blogs) => {
+  return blogs.length === 0
+    ? 0
+    : blogs.reduce((prev, curr) => prev + curr.likes, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  return blogs.length === 0
+    ? null
+    : blogs.reduce((prev, current) =>
+      prev.likes > current.likes ? prev : current
+    )
 }
 
 module.exports = {
-    dummy,totalLikes
+  dummy,
+  totalLikes,
+  favoriteBlog
 }
